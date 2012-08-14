2acd26d38531de7723fe15e60526cf4a60fe96c2
Jozef Dransfield <jdransfiel@wiley.com>
2012-08-14 17:18:28 +0100

    More work on the Model functionality

--- test/model-tests.js
+++ test/model-tests.js
@@ -7,16 +7,35 @@ module.exports = {
     },
     "Set and Get an attribute": function (test) {
         var model = new Gooey.Model();
-        model.set("att", "value");
-        test.same(model.get("att"), "value");
+	model.put("prop").set("value");
+        test.same(model.get("prop").get(), "value");
         test.done();
     },
     "Listen to property change events": function(test) {
         var model = new Gooey.Model();
-        model.on("change", function() {
+	model.put("prop").on("change", function() {
             test.done();
         });
-        model.set("att", "value")
+        model.get("prop").set("value");
+    }, 
+    "Events bubble up to parent models": function(test) {
+	var model = new Gooey.Model();
+	model.on("change", function() {
+            test.done();
+        });
+        model.put("prop").set("value");
+    },
+    "Event handler can cancel event bubbling": function(test) {
+	var model = new Gooey.Model();
+	model.on("change", function shouldNotBeCalledModelHandler() {
+            throw "Should not be called"
+        });
+        model.put("prop").on("change", function propertyChangeHandlerInTest() {
+            return false;
+        });
+
+        model.get("prop").set("value");
+	test.done();
     }
 
 }
